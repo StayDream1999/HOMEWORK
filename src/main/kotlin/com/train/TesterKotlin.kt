fdedec2e29ea164983c7afa2140b04a0eab2d670
StayDream1999 <StayDream1999@gmail.com>
2020-07-05 21:57:13 +0800

    homework1!

--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -1,2 +1,17 @@
 package com.train
 
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    print("Please enter number of ticket:")
+    var ticket =scanner.nextInt()
+    print(("How many roundtripp tickets:"))
+    var round =scanner.nextInt()
+    print("Total tickets:"+ticket)
+    print("Round trip:"+round)
+    print("Total:"+((ticket-round)*1000+(round*1800)))
+
+
+
+}
