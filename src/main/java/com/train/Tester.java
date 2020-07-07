abf436ce88323c0bc8ca2e2a4129d76ad2e3d4da
StayDream1999 <StayDream1999@gmail.com>
2020-07-08 01:56:25 +0800

    homework1!

--- src/main/java/com/train/Tester.java
+++ src/main/java/com/train/Tester.java
@@ -4,14 +4,21 @@ import java.util.Scanner;
 
 public class Tester {
     public static void main(String[] args) {
-        System.out.println("Please enter number of tikets:");
-        Scanner scanner=new Scanner(System.in);
-        int ticketnumber=scanner.nextInt();
-        System.out.println("How many round-trip tickets:");
-        int roundnumber=scanner.nextInt();
-        Ticket Tic =new Ticket(ticketnumber,roundnumber);
-        Tic.print();
+        int ticketnumber = -2;
+        Scanner scanner = new Scanner(System.in);
+        while (ticketnumber != -1){
+            System.out.println("Please enter number of tikets:");
+            ticketnumber = scanner.nextInt();
+            if (ticketnumber==-1){
+            break;}else {
+            System.out.println("How many round-trip tickets:");
+            int roundnumber = scanner.nextInt();
+            Ticket Tic = new Ticket(ticketnumber, roundnumber);
+            Tic.print();
+        }}
+        System.out.println("work done!!");
 
 
+        }
     }
-}
+
--- src/main/kotlin/com/train/TesterKotlin.kt
+++ src/main/kotlin/com/train/TesterKotlin.kt
@@ -4,14 +4,19 @@ import java.util.*
 
 fun main() {
     val scanner = Scanner(System.`in`)
-    print("Please enter number of ticket:")
-    var ticket =scanner.nextInt()
-    print(("How many roundtripp tickets:"))
-    var round =scanner.nextInt()
-    print("Total tickets:"+ticket)
-    print("Round trip:"+round)
-    print("Total:"+((ticket-round)*1000+(round*1800)))
-
-
-
+    var ticket = -2
+    while (ticket != -1) {
+        println("Please enter number of ticket:")
+        ticket = scanner.nextInt()
+        if (ticket == -1) {
+            println("work done!!")
+            break;
+        } else {
+            println(("How many roundtripp tickets:"))
+            var round = scanner.nextInt()
+            println("Total tickets:" + ticket)
+            println("Round trip:" + round)
+            println("Total:" + ((ticket - round) * 1000 + (round * 1800)))
+        }
+    }
 }
